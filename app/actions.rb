9b26f1471ed47d89e600cf1c83983554fae4ad7d
liveseasonal <matt@liveseasonal.com>
2016-06-11 19:25:30 +0000

    Submitting the message. The post action

--- app/actions.rb
+++ app/actions.rb
@@ -8,6 +8,11 @@ end
 #   'Message list here'
 # end
 
+get '/sayhello/:name' do
+  @name = params[:name]
+  erb :'messages/sayhello'
+end
+
 get '/messages' do
   @messages = Message.all
   #Need to load everything here before I can load into the erb file
@@ -16,11 +21,21 @@ end
 
 #Play around here. As this stands at the moment when I add a name in the url and the embedded code will run in the html file as it is required in the message. 
 
-get '/sayhello/:name' do
-  @name = params[:name]
-  erb :'messages/sayhello'
-end
+
 
 get '/messages/new' do
   erb :'messages/new'
 end  
+
+post '/messages' do 
+  binding.pry
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+
+  @message.save
+  redirect '/messages'
+end
+
